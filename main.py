e5d882038fa34517787adb8604b9bab87c2f68c8
JMGillum <JMGillum@protonmail.com>
2024-11-19 21:28:42 -0800

    Started translating

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from translations import romaji
 
-print("   H   K")
-for item in romaji:
-    print(f"{item}:",end=" ")
-    print(f'{romaji[item]["hiragana"]}  {romaji[item]["katakana"]}')
-    
+# Prints a chart of all translations
+# print("\tH\tK")
+# for item in romaji:
+#     print(f"{item}:",end=" ")
+#     print(f'\t{romaji[item]["hiragana"]}\t{romaji[item]["katakana"]}')
+
+input = "katakana sushi"
+print(input)
+word = ""
+trial = ""
+index = 0
+while index < (len(input)):
+    try:
+        trial = input[index:index+3]
+        test = romaji[trial]
+    except KeyError:
+        try:
+            trial = input[index:index+2]
+            test = romaji[trial]
+        except KeyError:
+            try:
+                trial = input[index:index+1]
+                test = romaji[trial]
+            except KeyError:
+                word = trial
+            else:
+                word = romaji[trial]["hiragana"]
+        else:
+            word = romaji[trial]["hiragana"]
+            index = index + 1
+    else:
+        word = romaji[trial]["hiragana"]
+        index = index + 2
+    print(word,end="")
+    index = index + 1
+
+print()
