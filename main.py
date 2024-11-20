edb067b2827a8c48cbbd02385589b4872e56e25b
JMGillum <JMGillum@protonmail.com>
2024-11-19 21:32:41 -0800

    Moved translation to a function

--- main.py
+++ main.py
@@ -6,34 +6,38 @@ from translations import romaji
 #     print(f"{item}:",end=" ")
 #     print(f'\t{romaji[item]["hiragana"]}\t{romaji[item]["katakana"]}')
 
-input = "katakana sushi"
-print(input)
-word = ""
-trial = ""
-index = 0
-while index < (len(input)):
-    try: # Checks if it is three letter kana
-        trial = input[index:index+3]
-        test = romaji[trial]
-    except KeyError:
-        try: # Checks if it is two letter kana
-            trial = input[index:index+2]
-            test = romaji[trial]
+def translate(input):
+    word = ""
+    trial = ""
+    index = 0
+    output = ""
+    while index < (len(input)):
+        try: # Checks if it is three letter kana
+            trial = input[index:index+3]
+            romaji[trial]
         except KeyError:
-            try: # Checks if it is one letter kana
-                trial = input[index:index+1]
-                test = romaji[trial]
+            try: # Checks if it is two letter kana
+                trial = input[index:index+2]
+                romaji[trial]
             except KeyError:
-                word = trial
-            else: # One letter kana
+                try: # Checks if it is one letter kana
+                    trial = input[index:index+1]
+                    romaji[trial]
+                except KeyError:
+                    word = trial
+                else: # One letter kana
+                    word = romaji[trial]["hiragana"]
+            else: # Two letter kana
                 word = romaji[trial]["hiragana"]
-        else: # Two letter kana
+                index = index + 1
+        else: # Three letter kana
             word = romaji[trial]["hiragana"]
-            index = index + 1
-    else: # Three letter kana
-        word = romaji[trial]["hiragana"]
-        index = index + 2
-    print(word,end="")
-    index = index + 1
+            index = index + 2
+        output = output + word
+        index = index + 1
+    return output
 
-print()
+if __name__ == "__main__":
+    input = "katakana sushi"
+    print(input)
+    print(translate(input))
